67b80de2eb888534abbbefb32f8b69998bbe2137
Shizanutik321 <craft.25@mail.ru>
2021-05-04 19:45:02 +0300

    add navbar locators

--- locators/locators.py
+++ locators/locators.py
@@ -20,7 +20,6 @@ class BasePageLocators:
     CART_ICON_BUTTON = (By.CSS_SELECTOR, "[title='Shopping Cart']")
     # CHECKOUT
     CHECKOUT_BUTTON = (By.CSS_SELECTOR, 'strong .fa.fa-share')
-
     # SEARCH
     SEARCH_TEXT_HOLDER = (By.CSS_SELECTOR, '.form-control.input-lg')
     SEARCH_BUTTON = (By.CSS_SELECTOR, '.btn.btn-default.btn-lg .fa')
@@ -28,7 +27,7 @@ class BasePageLocators:
     CART_BIG_BUTTON = (By.CSS_SELECTOR, '.btn.btn-inverse.btn-block.btn-lg.dropdown-toggle')
     CART_ITEMS_AND_TOTAL_COST = (By.CSS_SELECTOR, "#cart-total")
     CART_EMPTY_MESSAGE = (By.CSS_SELECTOR, '#cart .text-center')
-
+    # NAVBAR CATEGORIES
     NAVBAR_HOME_BUTTON = (By.CSS_SELECTOR, '.fa.fa-home')
     NAVBAR_DESKTOPS_BUTTON_DROPDOWN = '.dropdown-toggle[href="http://localhost/desktops"]'
     NAVBAR_DESKTOPS_PC_BUTTON = (By.CSS_SELECTOR, '#menu [href="http://localhost/desktops/pc"]')
