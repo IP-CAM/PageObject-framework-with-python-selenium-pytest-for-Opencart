6da92b239c1ae7a6eceb96ab38172fde77772852
Shizanutik321 <craft.25@mail.ru>
2021-05-06 16:42:54 +0300

    test_guest_can_go_to

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -10,3 +10,12 @@ class TestLogin:
         page.should_be_login_page()
         page.login()
         page.should_be_authorized()
+
+    def test_guest_can_go_to(self, browser):
+        page = LoginPage(browser)
+        page.open()
+        page.go_to_login_page_from_header()
+        page.go_to_register_page_from_login_page()
+        page.go_to_cart_page_from_header()
+        page.go_to_contact_us_page_from_header()
+        page.go_to_wishlist_page_from_header()
