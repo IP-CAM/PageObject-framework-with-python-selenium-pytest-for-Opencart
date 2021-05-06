cd9c2d9bff6f544f8cb44981451480dae77cbab3
Shizanutik321 <craft.25@mail.ru>
2021-05-06 12:27:33 +0300

    debug first test, now test runs and passes

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -4,13 +4,12 @@ from utilites.read_properties import ReadConfig
 
 
 class TestLogin:
-    url = ReadConfig.get_application_url()
-    email = ReadConfig.get_application_email()
-    password = ReadConfig.get_application_password()
-
-    def test_guest_can_login_from_login_page(self, browser, url, email, password):
-        page = LoginPage(browser, url)
+    def test_guest_can_login_from_login_page(self, browser):
+        url = ReadConfig.get_application_url()
+        email = ReadConfig.get_application_email()
+        password = ReadConfig.get_application_password()
+        page = LoginPage(browser, url=url)
         page.open()
         page.should_be_login_page()
-        page.login(email, password)
+        page.login(email=email, password=password)
         page.should_be_authorized()
