71f310ef361d0204344a48be6729785d706316fd
Shizanutik321 <craft.25@mail.ru>
2021-05-12 12:30:05 +0300

    add logger to tests

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -2,14 +2,16 @@ import pytest
 import allure
 from pages.login_page import LoginPage
 from utilites.read_properties import ReadConfig
-
-
-# Test configs
-existing_email = ReadConfig.get_application_email()
-valid_password = ReadConfig.get_application_password()
+from utilites.custom_logger import LogGen
 
 
 class TestLogin:
+    # Test configs
+    existing_email = ReadConfig.get_application_email()
+    valid_password = ReadConfig.get_application_password()
+
+    logger = LogGen.log_generator()
+
     @allure.feature("Login")
     @allure.story("Login from login page with email and password")
     @allure.severity('blocker')
@@ -17,12 +19,19 @@ class TestLogin:
         "email,password",
         [(existing_email, valid_password)])
     def test_guest_can_login(self, browser, email, password):
+        self.logger.info("********* TestLogin *********")
+        self.logger.info("********* Verifying 'Test guest can login' *********")
         page = LoginPage(browser)
         page.open()
+        self.logger.info("********* Open main page *********")
         page.go_to_login_page_from_header()
+        self.logger.info("********* Move to login page *********")
         page.should_be_login_page()
+        self.logger.info("********* Try to login with existing email and valid password *********")
         page.login(email, password)
         page.should_be_authorized()
+        self.logger.info("********* Authorization is successful *********")
+        self.logger.info("********* Test guest can login is passed *********")
 
     @allure.feature("Login")
     @allure.severity('critical')
