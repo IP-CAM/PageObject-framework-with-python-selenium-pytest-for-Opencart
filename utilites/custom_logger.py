ce4d944d61fce172e3189cccfe5bb8ebd70a3ff4
Shizanutik321 <craft.25@mail.ru>
2021-05-12 13:02:48 +0300

    fix logge

--- utilites/custom_logger.py
+++ utilites/custom_logger.py
@@ -4,10 +4,10 @@ import logging
 class LogGen:
     @staticmethod
     def log_generator():
-        logging.basicConfig(filename="..\\logs\\automation.log",
-                            format='%(asctime)s: %(levelname)s" %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p'
-                            )
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler = logging.FileHandler(filename='.\\Logs\\automation.log')
+        handler.setFormatter(formatter)
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
         return logger
